7d9f1dc337b969ac1358e8d5c5cf9727b73cfb60
InfinteEndeavours <ethan.pitt@protonmail.com>
2022-11-11 11:55:53 +0000

    Add Browser Query challenge

---
+++ browserQueries.js
@@ -0,0 +1,19 @@
+// To be executed in the Browser Console
+// All images, based on element type
+$('img');
+// The card_image class
+$('.class_card');
+// The footer, using its id
+$('.my_footer')
+// All paragraph tags within the footer
+$('.my_footer>p');
+// All header elemens (<h1> through <h6>)
+$(':header');
+// The first element on the page
+$('body:first');
+// Get the last div on the page
+$("div:last")
+// Get the last img on the page
+$("img:last");
+// Get all elements that have an attribute of href
+$("[href]")
